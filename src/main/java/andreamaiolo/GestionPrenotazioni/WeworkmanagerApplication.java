253f220247cc5a09f3923c4156390b3490a786ab
Andrea Maiolo <maiolo.m.2@gmail.com>
2025-07-18 10:23:53 +0200

    initial commit

---
+++ src/main/java/andreamaiolo/GestionPrenotazioni/WeworkmanagerApplication.java
@@ -0,0 +1,13 @@
+package andreamaiolo.GestionPrenotazioni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeworkmanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeworkmanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/andreamaiolo/GestionPrenotazioni/WeworkmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package andreamaiolo.GestionPrenotazioni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeworkmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
